134b95e9ee3b36a09d3394491c204a6d7b867f90
Daniel Uribe <duribe8@gmail.com>
2016-08-04 22:15:08 -0700

    Finished styling login pages including sessions#new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # Not sure if I should apply this without setting up Heroku email sender
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameters_sanitizer.for(:sign_up) << :name
+  end
 end
