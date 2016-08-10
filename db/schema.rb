8273c76443a35214014267ce43cc04e5db083d37
Daniel Uribe <duribe8@gmail.com>
2016-08-09 20:18:54 -0700

    Added Transactions Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809034637) do
+ActiveRecord::Schema.define(version: 20160810031508) do
 
   create_table "balances", force: :cascade do |t|
     t.float    "amount"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20160809034637) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_balances_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string   "name"
+    t.float    "amount"
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
