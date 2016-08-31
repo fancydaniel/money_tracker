44726ca994918c8540894e8f9e519b057c5e4dd1
Daniel Uribe <duribe8@gmail.com>
2016-08-30 22:14:12 -0700

    Dropped balances table (1st attempt did not work)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831043634) do
+ActiveRecord::Schema.define(version: 20160831051229) do
 
   create_table "accounts", force: :cascade do |t|
     t.decimal  "balance",    default: "0.0"
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 20160831043634) do
     t.datetime "updated_at",                 null: false
     t.index ["name"], name: "index_accounts_on_name"
     t.index ["user_id"], name: "index_accounts_on_user_id"
-  end
-
-  create_table "balances", force: :cascade do |t|
-    t.float    "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.index ["user_id"], name: "index_balances_on_user_id"
   end
 
   create_table "transactions", force: :cascade do |t|
